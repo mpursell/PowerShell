5d88843a0acf8511c7951bacf9791b6ab439c4d2
mpursell <michael.pursell@gmail.com>
2016-10-24 20:52:01 +0100

    Added function Expand-ZIPFile: Get-DATFile

--- DaaS/Get-DATFile.ps1
+++ DaaS/Get-DATFile.ps1
@@ -6,13 +6,17 @@
 
 function Get-DATFiles{
 
+    # set the ftp server url and call the function to get the files 
+    # on the ftp server.
+
     $folderPath="ftp://ftp.mcafee.com/commonupdater/"
     $files=Get-FTPDir $folderPath
 
     
+    # ftp file list is returned as one long string with only the 
+    # length available as a property - so do a regex that finds 
+    # all substrings that match avvdat-****.zip
     $DATList = @()
-
-
     $files.ToString() | Select-String -Pattern '(avvdat\W\d\d\d\d\W\w\w\w)' -AllMatches | 
     
         foreach{
@@ -26,6 +30,8 @@ function Get-DATFiles{
         } | Out-Null
 
 
+    # get the list of matches and call the function to 
+    # download the matched files
     foreach($DAT in $DATList){
 
         
@@ -35,6 +41,9 @@ function Get-DATFiles{
 }
 
 function Get-FtpDir ($url) {
+
+    # get the list of files available on  the ftp server
+
     $request = [Net.WebRequest]::Create($url)
     $request.Method = [System.Net.WebRequestMethods+FTP]::ListDirectory
     #if ($credentials) { $request.Credentials = $credentials }
@@ -48,19 +57,6 @@ function Get-FtpDir ($url) {
 
 function Download-DATFiles($DATFile, $folderPath){
 
-<#
-        $source = $folderpath + $DATFile
-        $destination = "C:\Users\mike\Documents\Test\"
-        $username = ""
-        $password = ""
-
-        $WebClient = New-Object System.Net.WebClient
-        $WebClient.Credentials = New-Object System.Net.NetworkCredential($username, $password)
-        
-	    $WebClient.DownloadFile($source, $destination)
-       
-#>
-
     $source = $folderpath + $DATFile
 
     # make sure that IE is set to open zip files automatically from Downloads page
@@ -70,10 +66,26 @@ function Download-DATFiles($DATFile, $folderPath){
     
     Start-Sleep -Seconds 60
     
+    # IE will automatically cache the zip files if IE is setup correctly,
+    # so we need a copy-item to copy the zipped files from temp cache
+    # to permanent location 
     Copy-Item -Path "C:\Users\mike\AppData\Local\Microsoft\Windows\INetCache\IE\XV0F1EOW\$DATFile"  -Destination "C:\users\mike\Documents\test\"
         
     
 
+}
+
+function Extract-ZIPFile($file, $destination)
+{
+    $shell = new-object -com shell.application
+    $zip = $shell.NameSpace($file)
+
+    foreach($item in $zip.items())
+    {
+
+        $shell.Namespace($destination).copyhere($item)
+    
+    }
 }
 
 
